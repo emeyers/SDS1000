f0718002eb1d6ab6b58c9f704b924b994e2b3511
Ethan Meyers <ethan.meyeres@gmail.com>
2025-08-14 23:09:46 -0400

    Moved functions from list_class_files.R to download_functions.R

--- R/download_functions.R
+++ R/download_functions.R
@@ -268,12 +268,8 @@ download_any_file <- function(file_path_and_name, force_download = FALSE, mode =
 
 
 
-### Helper functions -----------------------------------------------------
-
-
-
-#' A helper function that downloads a file if it does not already exist. 
-#' If the file already exists, a warning message is shown (if desired).
+#' A helper function that downloads a file only if it does not already exist.
+#' If the file already exists, a message is shown (if desired).
 #'
 #' @param save_file_name The name of the file to save the downloaded file as.
 #' @param git_full_path The full path to the file on GitHub.
@@ -319,6 +315,60 @@ download_nonexistent_file <- function(save_file_name, git_full_path, mode = "wb"
   }
   
 }
+
+
+
+
+
+#' Lists the files that are on the class GitHub site in a particular directory
+#'
+#' @param file_dir_name The name of a directory in the GitHub repository.
+#'
+#' @examples
+#'  # List files in the data directory on GitHub 
+#'  \dontrun{list_class_files("homework/homework_-1")}
+#'
+#' @export
+list_github_files <- function(file_dir_name) {
+  
+  # newer repositories use the term "main" for the default repository branch
+  file_url <- paste0("https://api.github.com/repos/", get_github_user_name(), "/",
+                     get_package_name(), "/git/trees/", get_branch_name(), "?recursive=1")
+  
+  
+  req <- httr::GET(file_url)
+  stop_for_status(req)
+  
+  
+  filelist <- unlist(lapply(httr::content(req)$tree, "[", "path"), use.names = F)
+  file_names <- grep(paste0("ClassMaterial/", file_dir_name, "/"),
+                     filelist, value = TRUE, fixed = TRUE)
+  
+  basename(file_names)
+  
+}
+
+
+
+
+
+
+
+# # Helper function that checks if a given file exists in the file_dir_name on the
+# # class github page. 
+# 
+# # Example: check_github_file_exists("homework/homework_-1", "test_data.rda")
+# 
+# check_github_file_exists <- function(file_dir_name, file_name) {
+#   
+#   file_names <- list_class_files(file_dir_name)
+#   
+#   if (!(file_name %in% file_names)) {
+#     stop(paste("The file",  file_name,
+#                "does not exit in the class GitHub repository."))
+#   }
+#   
+# }
 
 
 
